4b2cee3dcffc56b54d05dfc6e22cf6168bd3c604
neketeli <neketli.cspl@gmail.com>
2023-02-15 11:40:31 +0300

    Update

--- src/components/FeaturedProducts/index.tsx
+++ src/components/FeaturedProducts/index.tsx
@@ -16,8 +16,8 @@ function FeturedProducts({
   productList,
 }: Props) {
   return (
-    <section id={sectionId} className="feturedProducts py-8 px-2">
-      <div className="flex items-center justify-between">
+    <section id={sectionId} className="feturedProducts  py-8 px-2">
+      <div className="mb-4 flex items-center justify-between">
         {title && <h2 className="text-xl font-bold">{title}</h2>}
         {description && (
           <p className="max-w-[66%] text-base text-gray-500">{description}</p>
