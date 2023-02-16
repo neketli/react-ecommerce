22653aacffd336f3b5dc609d1842cc04a69f68a4
neketeli <neketli.cspl@gmail.com>
2023-02-16 16:15:43 +0300

    Update about section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,21 +8,21 @@ const About = () => {
           <div>
             <div className="h-64 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-auto">
               <img
-                src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600&h=750"
+                src="/src/assets/img/about.jpg"
                 loading="lazy"
-                alt="Photo by Martin Sanchez"
+                alt="woman"
                 className="h-full w-full object-cover object-center"
               />
             </div>
           </div>
 
           <div className="md:pt-8">
-            <p className="text-center font-bold text-indigo-500 md:text-left">
-              Who we are
+            <p className="text-center font-bold text-yellow-500 md:text-left">
+              Кто мы?
             </p>
 
-            <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 sm:text-3xl md:mb-6 md:text-left">
-              Our competitive advantage
+            <h2 className="mb-4 text-center text-2xl font-semibold text-gray-800 sm:text-3xl md:mb-6 md:text-left">
+              Информация о компании
             </h2>
 
             <p className="mb-6 text-gray-500 sm:text-lg md:mb-8">
@@ -32,6 +32,14 @@ const About = () => {
               display a sample of fonts or generate text for testing. Filler
               text is dummy text which has no meaning however looks very similar
               to real text.
+            </p>
+
+            <h2 className="mb-2 text-center text-xl font-bold text-gray-800 sm:text-2xl md:mb-4 md:text-left">
+              Продаем качественное оборудование
+            </h2>
+
+            <p className="mb-6 text-gray-500 sm:text-lg md:mb-8">
+              Здесь пару слов о том почему стоит нам доверять
               <br />
               <br />
               This is a section of some simple filler text, also known as
@@ -39,19 +47,6 @@ const About = () => {
               text but is or otherwise generated. It may be used to display a
               sample of fonts or generate text for testing. Filler text is dummy
               text which has no meaning however looks very similar to real text.
-            </p>
-
-            <h2 className="mb-2 text-center text-xl font-semibold text-gray-800 sm:text-2xl md:mb-4 md:text-left">
-              About us
-            </h2>
-
-            <p className="mb-6 text-gray-500 sm:text-lg md:mb-8">
-              This is a section of some simple filler text, also known as
-              placeholder text. It shares some characteristics of a real written
-              text but is random or otherwise generated. It may be used to
-              display a sample of fonts or generate text for testing. Filler
-              text is dummy text which has no meaning however looks very similar
-              to real text.
             </p>
           </div>
         </div>
